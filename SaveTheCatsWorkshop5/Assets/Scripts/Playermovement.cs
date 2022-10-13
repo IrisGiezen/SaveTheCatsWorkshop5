7529ae5d373a698ca5af20bcef167f7613cdf751
Iris Giezen <IJG.Giezen@student.han.nl>
2022-10-13 12:58:08 +0200

    Pickup Food

--- SaveTheCatsWorkshop5/Assets/Scripts/Playermovement.cs
+++ SaveTheCatsWorkshop5/Assets/Scripts/Playermovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -32,7 +30,6 @@ public class Playermovement : MonoBehaviour
 
     void Update()
     {
-
         if (characterController.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
@@ -52,7 +49,7 @@ public class Playermovement : MonoBehaviour
         if (Input.GetButton("Fire2"))
         {
             anim.SetTrigger("attacking");
-            Instantiate(Claw, transform.position + new Vector3(0,0.2f,0.35f), transform.rotation);
+            Instantiate(Claw, transform.position + new Vector3(0, 0.2f, 0.35f), transform.rotation);
         }
 
 
@@ -71,7 +68,39 @@ public class Playermovement : MonoBehaviour
 
         forwardInput = movementVector.y;
         sidewardInput = movementVector.x;
-
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            Debug.Log("Collision Food Detected");
+            Destroy(collision.gameObject);
+            speed += 2;
+        }
+
+        if (collision.gameObject.CompareTag("NotFood"))
+        {
+            Debug.Log("Collision Not Food Detected");
+            Destroy(collision.gameObject);
+            speed -= 2;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Food"))
+        {
+            Debug.Log("Collider Food Detected");
+            Destroy(collider.gameObject);
+            speed += 2;
+        }
+
+        if (collider.gameObject.CompareTag("NotFood"))
+        {
+            Debug.Log("Collider Not Food Detected");
+            Destroy(collider.gameObject);
+            speed -= 2;
+        }
+    }
 }
